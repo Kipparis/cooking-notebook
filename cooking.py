a8f6e40d76319edc1584c219a710113d0235bad1
Aleksey <keepintine@gmail.com>
2021-03-29 01:36:22 +0300

    write command line arguments

--- cooking.py
+++ cooking.py
@@ -1,11 +1,15 @@
 #!python3
+# -*- coding: utf-8 -*-
 
 import os, sys
-import pandas
+import pandas as pd
 import datetime
 import argparse
 
-from utils.recipe import Recipe
+from peewee import *
+from utils.models import *
+
+from utils import recipe
 
 # ========== Print to stderr ===========
 def eprint(*args, **kwargs):
@@ -47,6 +51,29 @@ parser.add_argument('--working-dir',
         type    = str,
         help    = "file to which recipes and buy list must be outputted",
         dest    = "working_dir")
+parser.add_argument('-l', '--list',
+                    action  = "store_true",
+                    help    = "list all recipes",
+                    dest    = "list")
+parser.add_argument('-a', '--add',
+                    action = "store_true",
+                    help    = "add new recipe to db",
+                    dest    = "add")
+parser.add_argument('-d', '--database',
+                    default = "recipe.db",
+                    type    = str,
+                    help    = "database for recipe storage",
+                    metavar = "DB_FN",
+                    dest    = "db_fn")
+parser.add_argument('-e', '--export',
+                    action = "store_true",
+                    help = "export database into several csv files (exported and imported files must have same versions)",
+                    dest = "export_flag")
+parser.add_argument('-i', '--import',
+                    action = "store_true",
+                    help = "import database from serveral csv files (exported and imported files must have same versions)",
+                    dest = "import_flag")
+
 args = parser.parse_args()
 
 def concat_recipe():
@@ -163,5 +190,81 @@ def concat_recipe():
 
 
 if __name__ == "__main__":
+    # create database if it not exists
+    create_database()
+
     if args.do_concat_recipe:
         concat_recipe()
+
+    if args.export_flag:
+        print("exporting database contents")
+        export_database()
+
+    if args.import_flag:
+        print("importing database contents")
+
+    if args.add:    # add recipe(s)
+        MealType.output_choices()
+        meal = input("select meal type or enter new: ")
+        #
+        # select\create meal type
+        #
+        if meal.isdigit(): # user input is number
+            meal = MealType.get(MealType.id == int(meal)).name
+        #
+        # create recipe
+        #
+        recipe = str(input("enter recipe name: "))
+        #
+        # insert cooking algorithm
+        #
+        print("enter algorithm line by line (empty line considered as empty)")
+        algo = []
+        while True:
+            line = input()
+            if line in "":
+                break
+            algo.append(line)
+        #
+        # enter ingredients
+        #
+        ingredients = {}
+        while True:
+            # read ingredient name
+            ingredient = input("select ingredient or type new: ")
+            # user want to exit
+            if ingredient in "exit":
+                break
+            quantity = float(input("enter quantity: "))
+            MeasureUnit.output_choices()
+            measure_unit = input("enter measure units (int or str): ")
+            if measure_unit.isdigit():
+                measure_unit = MeasureUnit.get(MeasureUnit.id == int(measure_unit)).name
+            ingredients[ingredient] = [quantity, measure_unit]
+        Recipe.create_new(recipe, meal, algo, ingredients)
+
+    if args.list:
+        # build query joining all recipes
+        query = (Recipe
+                 .select(Recipe, MealType, RecipeIngredient, Ingredient, MeasureUnit)
+                 .join(MealType)
+                 .switch(Recipe)
+                 .join(RecipeIngredient,
+                       on=(RecipeIngredient.recipe == Recipe.id),
+                       attr='ri')
+                 .join(Ingredient,
+                       on=(RecipeIngredient.ingredient == Ingredient.id),
+                       attr='ingr')
+                 .switch(RecipeIngredient)
+                 .join(MeasureUnit))
+        if len(query) != 0:
+            # output contents of query
+            df = pd.DataFrame([{"name": entry.name,
+                                "meal type": entry.meal_type.name,
+                                "ingr name": entry.ri.ingr.name,
+                                "ingr qty": entry.ri.quantity,
+                                "measure unit": entry.ri.measure_unit.name}
+                            for entry in query])
+            print(df)
+        else:
+            print("no recipes in the book")
