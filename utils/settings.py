741e06030a1bc48683121f8666a9d899c66d035e
Aleksey <keepintine@gmail.com>
2023-09-09 17:07:58 +0300

    create custom env importer

--- utils/settings.py
+++ utils/settings.py
@@ -1,7 +1,13 @@
 import os
 
+def get_env_var(env_name):
+    if env_name in os.environ:
+        return os.environ[env_name]
+    print(f"WARN: {env_name} is not in environments")
+    return ""
+
 # api.nal.usda.gov
-api_key = os.environ['NAL_USDA_GOV_API_KEY']
+api_key = get_env_var('NAL_USDA_GOV_API_KEY')
 
 # nutrient log file
 NUTRIENTS_LOG_FILE = open("log/nutrients.log", "w")
