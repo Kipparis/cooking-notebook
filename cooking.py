5cffe0b1044db5961c9e478877f9d5562d7f094c
Aleksey <keepintine@gmail.com>
2023-09-09 23:23:11 +0300

    strict calculation by recipes flag

--- cooking.py
+++ cooking.py
@@ -29,12 +29,23 @@ parser.add_argument('--working-dir',
                     type    = str,
                     help    = "directory with recipes",
                     dest    = "working_dir")
+parser.add_argument('--recipes',
+                    default = None,
+                    metavar = "RECIPE1,RECIPE2",
+                    type    = str,
+                    help    = "comma-delimited recipe names",
+                    dest    = "recipe_names")
 parser.add_argument('--list', '-l',
                     action  = "store_true",
                     help    = "only list recipe names",
                     dest    = "do_list_recipes")
 
 args = parser.parse_args()
+if args.recipe_names:
+    print(f"DEBUG: remove spaces from '{args.recipe_names}'")
+    args.recipe_names = args.recipe_names.replace(", ", ",").replace(" ,", ",").strip()
+    print(f"DEBUG: result '{args.recipe_names}'")
+
 
 if __name__ == "__main__":
     print("hello")
@@ -52,6 +63,10 @@ if __name__ == "__main__":
                 print(f"WARN: duplicate recipe_names: {recipe_data['name']}")
                 continue
             print(f"DEBUG: adding {recipe_data['name']} into resulting list of recipe names")
+            if args.recipe_names and recipe_data["name"] not in args.recipe_names.split(","):
+                print(f"INFO: skipping {recipe_data['name']} because it is not"
+                      " specified in --recipes flag")
+                continue
             recipe_names.add(recipe_data["name"])
             yield recipe_data
 
