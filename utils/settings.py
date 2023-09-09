68fa845a8f167bb5518d21f74b02ec6b8db88971
Aleksey <keepintine@gmail.com>
2023-09-09 17:08:11 +0300

    remove unused strings

--- utils/settings.py
+++ utils/settings.py
@@ -9,8 +9,7 @@ def get_env_var(env_name):
 # api.nal.usda.gov
 api_key = get_env_var('NAL_USDA_GOV_API_KEY')
 
-# nutrient log file
-NUTRIENTS_LOG_FILE = open("log/nutrients.log", "w")
+base_mu = ["g", "mg", "mcg"]    # conves to which I parse
 
 
 base_mu = ["g", "mg", "mcg"]    # conves to which I parse
